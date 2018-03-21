00e72001bec6ae087dbf6dbf2c76ee9c5f521875
Sebastian Munoz <sebasmunoz694@gmail.com>
2018-03-20 21:48:24 -0600

    fin dia 2

--- script.js
+++ script.js
@@ -18,27 +18,57 @@ doctorFB = $.map(doctorFB, function(value, index) {
 // Request de firebase de nombres de pacientes
 var pacientesFB = {
     "1" : {
-      "name" : "juanito hola",
+      "firstName" : "juanito",
+      "lastName" : "Perez",
+      "FbUser" : "prueba",
+      "phone": "442 204 5766",
+      "email": "nutriologo@gmail.com",
+      "altura": "180",
+      "peso": "70",
+      "abdomen": "90",
+      "brazo": "40",
+      "cintura": "90",
+      "id": "1239asda42"
     },
     "2" : {
-      "name" : "pepe prueba",
+      "firstName" : "pepe",
+      "lastName" : "prueba",
+      "FbUser" : "prueba",
+      "phone": "442 204 5766",
+      "email": "nutriologo@gmail.com",
+      "altura": "180",
+      "peso": "70",
+      "abdomen": "90",
+      "brazo": "40",
+      "cintura": "90",
+      "id": "123vc42"      
     },
     "3" : {
-      "name" : "pepe prasda"
+      "firstName" : "pepe",
+      "lastName" : "prasda",
+      "FbUser" : "prueba",
+      "phone": "442 204 5766",
+      "email": "nutriologo@gmail.com",
+      "altura": "180",
+      "peso": "70",
+      "abdomen": "90",
+      "brazo": "40",
+      "cintura": "90",
+      "id": "123we342"    
     },
     "4" : {
-      "name" : "pepe prassda"
-    },
-    "5" : {
-      "name" : "pepe pancho"
-    },
-    "120" : {
-      "name" : "Enrique"
-    },
-    "123" : {
-      "name" : "Enasdque"
-    }
-
+      "firstName" : "pepe",
+      "lastName" : "praassda",
+      "FbUser" : "prueba",
+      "phone": "442 204 5766",
+      "email": "nutriologo@gmail.com",
+      "altura": "180",
+      "peso": "70",
+      "abdomen": "90",
+      "brazo": "40",
+      "cintura": "90",
+      "id": "123weaaw"
+  	}
   }
 
 pacientesFB = $.map(pacientesFB, function(value, index) {
@@ -72,7 +102,7 @@ function perfilDR(){
 			'</div>'+
 			'<div class="row">'+
 			  '<div class="col-12 centrarPerfil">'+
-			    '<button type="button" class="btn btn-secondary" onclick=" modificarPerfil()">Modificar</button>'+
+			    '<button type="button" class="btn btn-secondary" onclick=" modificarPerfilDr()">Modificar</button>'+
 			  '</div>'+
 			'</div>'
 
@@ -93,10 +123,13 @@ function perfilDR(){
 	document.getElementById("emailDr").value = doctorFB[0].email
 	document.getElementById("passwordDr").value = doctorFB[0].password
 	document.getElementById("direccionDr").value = doctorFB[0].address
-	
+
+	//pintar boton seleccionado
 	document.getElementById("selectPerfil").style.backgroundColor = '#e6e6f9';
 	document.getElementById("selectDietas").style.backgroundColor = 'white';	
 	document.getElementById("selectPacientes").style.backgroundColor = 'white';
+
+	//ocultar otras pestanas
 	document.getElementById("miPerfil").style.display = "block";
 	document.getElementById("pacientes").style.display = "none";
 	document.getElementById("sinSeleccionPaciente").style.display = "none";
@@ -105,7 +138,7 @@ function perfilDR(){
 
 }
 
-function modificarPerfil(){
+function modificarPerfilDr(){
     var imagenDr = document.getElementById("imagenDrURL").value;
     var nameDR = document.getElementById("nameDr").value;
     var lastDR = document.getElementById("lastDr").value;
@@ -125,25 +158,29 @@ function opcionPacientes(){
 	function tarjetasNombres(){
 
 		for (var i = 0; i < pacientesFB.length; i++) {
+
 			var pintarNombres =
-			'<div class="row indiceNombres">'+
+			'<div class="row indiceNombres" id="seleccionado'+pacientesFB[i].id+'">'+
 	            '<div class="cuadroNombre">'+
-	              '<h3 id="name'+i+'"></h3>'+
+	              '<h3 id="firstName'+i+'" class="espacioNombre"></h3>'+
+	              '<h3 id="lastName'+i+'"></h3>'+
 	            '</div>'+
 	            '<div class="opciones">'+
 	              '<div class="botonOpcion">'+
-	                '<button type="button" class="btn btn-primary">Perfil</button>'+
+	                '<button type="button" class="btn btn-primary" value="'+pacientesFB[i].id+'" onclick="perfilPaciente(this)">Perfil</button>'+
 	              '</div>'+
 	              '<div class="botonOpcion">'+
-	                '<button type="button" class="btn btn-secondary">Resultados</button>'+
+	                '<button type="button" class="btn btn-secondary" value="'+pacientesFB[i].id+'" onclick="resultadosPaciente()">Resultados</button>'+
 	              '</div>'+
 	            '</div>'+
 	          '</div>'+
 	        '</div>'
 
 	    $("#pacientes").append(pintarNombres)
+		$("#firstName"+i).html(pacientesFB[i].firstName)
+		$("#lastName"+i).html(pacientesFB[i].lastName)
 
-		$("#name"+i).html(pacientesFB[i].name)
+
 
 		}
 	}
@@ -153,15 +190,69 @@ function opcionPacientes(){
 		contadorPacientes = contadorPacientes+1
 	}
 
+
+	//pintar boton seleccionado
 	document.getElementById("selectPerfil").style.backgroundColor = 'white';
 	document.getElementById("selectDietas").style.backgroundColor = 'white';	
 	document.getElementById("selectPacientes").style.backgroundColor = '#e6e6f9';
+
+	//ocultar otras pestanas
 	document.getElementById("pacientes").style.display = "block";
 	document.getElementById("miPerfil").style.display = "none";
 	document.getElementById("sinSeleccionPaciente").style.display = "flex";
 	document.getElementById("dietas").style.display = "none";
 
 }
+
+	// perfil pacientes
+function perfilPaciente(event){
+
+	var idPaciente = event.value
+
+	var infoPT  
+
+	var pintarPerfilPaciente = 
+		'<div class="row container">'+
+		  '<div class="col-6 centrarPerfil">'+
+		    '<h4 class="campoPaciente">Datos Personales:</h4>'+
+		  '</div>'+
+		  '<div class="col-6 centrarPerfil">'+
+		    '<h4 class="campoPaciente">Medidas:</h4>'+
+		  '</div>'+
+		'</div>'+
+		'<div class="row container">'+
+		  '<div class="col-6 centrarPerfil">'+
+		    '<h5 class="campoPaciente">Nombre: <input type="text" id="namePT" value=""></input></h5>'+
+		    '<h5 class="campoPaciente">Apellido: <input type="text" id="lastPT" value=""></input></h5>'+
+		    '<h5 class="campoPaciente">FB User: <input type="text" id="fbUserPT" value=""></input></h5>'+
+		    '<h5 class="campoPaciente">Teléfono: <input type="text" id="phonePT" value=""></input></h5>'+
+		    '<h5 class="campoPaciente">Correo electronico: <input type="text" id="emailPT" value=""></input></h5>'+
+		  '</div>'+
+		  '<div class="col-6 centrarPerfil">'+
+		    '<h5 class="campoPaciente">Altura cm: <input type="password" id="alturaPT" value=""></input></h5>'+
+		    '<h5 class="campoPaciente">Peso kg: <input type="text" id="pesoPT" value=""></input></h5>'+
+		    '<h5 class="campoPaciente">Abdomen cm: <input type="text" id="abdomenPT" value=""></input></h5>'+
+		    '<h5 class="campoPaciente">Crazo cm: <input type="text" id="brazoPT" value=""></input></h5>'+
+		    '<h5 class="campoPaciente">Cintura cm: <input type="text" id="cinturaPT" value=""></input></h5>'+
+		  '</div>'+
+		'</div>'+
+		'<div class="row container">'+
+		  '<div class="col-12 centrarPerfil">'+
+		    '<button type="button" class="btn btn-secondary" onclick=" modificarPerfil()">Modificar</button>'+
+		  '</div>'+
+		'</div>'
+
+	$("#perfilPaciente").append(pintarPerfilPaciente)
+
+
+	//ocultar otras pestanas
+	document.getElementById("sinSeleccionPaciente").style.display = "none";
+	document.getElementById("perfilPaciente").style.display = "block";
+
+}
+
+//-----------------------------------
+	//Dietas
 
 var contadorDietas = 0
 
@@ -172,9 +263,12 @@ function opcionDietas(){
 		contadorDietas = contadorDietas+1
 	}
 
+	//pintar boton seleccionado
 	document.getElementById("selectPerfil").style.backgroundColor = 'white';
 	document.getElementById("selectDietas").style.backgroundColor = '#e6e6f9';	
 	document.getElementById("selectPacientes").style.backgroundColor = 'white';
+
+	//ocultar otras pestanas
 	document.getElementById("dietas").style.display = "block";
 	document.getElementById("miPerfil").style.display = "none";
 	document.getElementById("pacientes").style.display = "none";
